4277b842e2e9a863134c157097043828080510c5
arvmpdm <arturo.vazquez.dev@gmail.com>
2025-08-09 20:34:19 -0600

    Hola mundo en react

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports={
+    mode:"development",
+    entry:[
+        __dirname + "/app/App.js",
+
+    ],
+    output:{
+        path: __dirname + "/public",
+        filename: "bundle.js",
+        publicPath: "/public"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                loader: "babel-loader",
+                options:{
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    plugins: ["@babel/plugin-proposal-class-properties",
+                        "@babel/plugin-proposal-export-default-from",
+                        "react-hot-loader/babel"
+                    ]
+                }
+               
+            }
+        ]
+    }
+}
